a3e9c32fe44f2ed4cbf26fee066050553832ec55
Antoine Blosse <ablosse@ucsd.edu>
2019-10-15 21:05:45 -0700

    slicing footnotes list into unique list per table

--- HTML_file_test.py
+++ HTML_file_test.py
@@ -12,8 +12,8 @@ soup = BeautifulSoup(html_file, 'html.parser')
 # removing links and bolded text tags, and their contents
 for b in soup('b'):
     b.decompose()
-for a in soup('a'):
-    a.decompose()
+# for a in soup('a'):
+#     a.decompose()
 div = soup.find('div')
 # print(div.prettify())
 
@@ -72,7 +72,7 @@ data_with_headers = []
 
 for row in div.find_all('tr'):
     # print(row)
-    # # need to print headers before data
+    # # # need to print headers before data
     # if "class == 'colsep0'" in row:
     #     header = div.find_all('tr', class_='colsep0')
     #     print(header)
@@ -83,8 +83,8 @@ for row in div.find_all('tr'):
     #     header = header.text
     #     # header = [ele.text.strip() for ele in header]
     #     print(header)
-    headers = div.find_all('th', class_='colsep0 rowsep0')
-    headers = [ele.text.strip() for ele in headers]
+    # headers = div.find_all('th', class_='colsep0 rowsep0')
+    # headers = [ele.text.strip() for ele in headers]
     # print(header)
     # data.append(header)
     # if header is None:
@@ -100,9 +100,69 @@ for row in div.find_all('tr'):
     # # cols = [ele.text.strip() for ele in columns]
     # data_with_headers.append(cols)
 
+# print()
 # print(data)
-dftable = pd.DataFrame(data)
+# dftable = pd.DataFrame(data)
 # print(dftable)
+
+# # # if last letter in header string is a,b,c,d,e; replace with text in footnotes  # # #
+headers = div.find_all('th', class_='colsep0 rowsep0')
+headers = [ele.text.strip() for ele in headers]
+# print(headers)
+
+for i, header in enumerate(headers):
+    headers[i] = header.replace(" ()", "")
+    n_lists = headers.count("")         # temporary replacement for table_title[i]
+    # for n in range(n_lists):
+    #     split_headers = headers.split("")
+#     # parts = headers.split('')
+#     # print(parts)
+#     if header == "":
+#         print('ok')
+#         np.split(headers, [0, i])
+# print(n_lists)
+
+
+# print()
+# print(headers)
+
+header1 = headers[1]
+if header1.endswith(('a', 'b', 'c', 'd')):
+    header1 = header1[:-1]
+    header1 = header1 + " (125 MHz)"
+# print(header1)
+
+
+footnotes = div.find_all('div', class_="footnote")
+footnotes = [ele.text.strip() for ele in footnotes]
+# print(footnotes.prettify())
+print(footnotes)
+print()
+temp_headers = []
+temp_footnotes = []
+rest_footnotes = footnotes
+temp_data = []
+start = 0
+
+# # # split footnotes list into smaller lists, one for each NMR table
+# for i, header in enumerate(headers):
+for i, footnote in enumerate(footnotes):
+    # if i == (len(footnotes) - 1):
+    #     print('+++++++++++++++')
+    if i > 0 and footnote.startswith('a') or i == (len(footnotes) - 1):
+        # if footnote == footnotes[0]: continue               # ignore first empty list
+        # print('++++++++++' + footnote + '++++++++++++' + '  ' + str(i))
+        temp_footnotes = footnotes[start:i]
+        rest_footnotes = rest_footnotes[i:]
+        if i == (len(footnotes) - 1):
+            # print('////////')
+            temp_footnotes = footnotes[start:]
+        start = i                                           # previous final index for temp_footnotes
+        print(temp_footnotes)
+        print()
+        # for i, temp_footnote in enumerate(temp_footnotes):
+        #   if temp_footnotes.startswith('a') & temp_headers.endswith('a'):
+        #       pass
 
 
 # # # First NMR Table
@@ -113,18 +173,19 @@ table_title = table_title.replace(" ()", "")
 ########################################################################################################################
 # # # Create CSV files for each NMR Table # # #
 csv_path = create_folders() + f"/{table_title}.csv"
-dftable.to_csv(csv_path, header=True, encoding='utf-8-sig')
+# dftable.to_csv(csv_path, header=True, columns=headers, encoding='utf-8-sig')
 ########################################################################################################################
 
 # headers = div.find_all('th', class_='colsep0 rowsep0')
 # headers = [cell.text.strip() for cell in headers]
 # headers = [cell.replace(" ()", "") for cell in headers]
-# for header in headers:
-#     if header == "":
-#         np.split(headers)
 # print(headers)
 
 # TO-DO:
-    # need to fill columns and rows of a csv, each cell comma-separated
-    # headers for each different table
+    # title: table_title for each different table
+    # headers: headers for each different table
+        # figure out how to split "headers" list
+        # footnotes: for each NMR table, replace headers hyperlink a,b,c,etc. with footnote at end of table
+    # fill columns and rows of a csv, each cell comma-separated
     # separate different tables (new csv/table)
+    # if/else: create new csv only if table_title contains "NMR"
